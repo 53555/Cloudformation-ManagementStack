64c553cb04b9532cea6181d330adff21e1cc6869
Silambarasan Karthikeyan <silambarasankarthikeyan@fico.com>
2021-01-27 20:51:02 +0530

    updated jenkins upload script

--- jenkins-upload.py
+++ jenkins-upload.py
@@ -6,13 +6,14 @@ import os
 import boto3
 
 bucket_name = "dev-ldap-jenkins-backup"
-backup_file_cmd = "aws --profile admin1 s3 ls s3://"+bucket_name+"/| sort -nr | head -1 | awk '{print $4}'"
+backup_file_cmd = "aws s3 ls s3://"+bucket_name + \
+    "/| sort -nr | head -1 | awk '{print $4}'"
 Jenkins_home = "/var/lib/jenkins/"
 
 backup_file = sp.check_output(backup_file_cmd, shell=True)
 backup_file = backup_file.decode("utf-8").split()[0]
 print(backup_file)
-copy_cmd = "aws --profile admin1 s3 cp s3://"+ bucket_name +"/"+ backup_file + " /tmp/"
+copy_cmd = "aws s3 cp s3://" + bucket_name + "/" + backup_file + " /tmp/"
 try:
     upload = sp.check_output(copy_cmd, shell=True)
     print("Upload to /tmp dir successfully")
@@ -22,7 +23,6 @@ try:
     print("Upload completed successfully")
 except Exception as e:
     print("upload failed \n" + e)
-
 
 
 
